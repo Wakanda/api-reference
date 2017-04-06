e9c083e241fe64ef846e72200150b3da6d4c6d66
Yann <yann.jego@wakanda.io>
2017-04-06 12:01:50 +0200

    Add handshake doc for websocket

--- src/api/httpServer.d.ts
+++ src/api/httpServer.d.ts
@@ -101,10 +101,13 @@ interface HttpServer {
      * // ./websocket-greetings.js
      * // Same as for ShareWorker
      * // Called every time a new websocket is connected
-     * onconnect = function ( msg ) {
+     * onconnect = function ( event ) {
      * 
      *     // Get the websocket port
-     *     var wsPort = msg.ports[0];
+     *     var wsPort = event.ports[0];
+     *     // Get the websocket handshake data
+     *     var client = event.client;
+     *     // Is available: client.id, client.port, client.urlPath, client.headers
      * 
      *     // Called every time a client sends a message    
      *     wsPort.onmessage = function( message ) {
